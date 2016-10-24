6d10e584628eb3d64949f15d4a88b5ef2ddd2101
xz1148 <xz1148@york.ac.uk>
2016-10-24 12:41:44 +0100

    calculation of r - ro is added

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ freq = 300.0e6   # frequency, the float number
 print 'The frequency is %7.2f MHz' % (freq / 1e6)
 lambda0 = EMConst.c0 / freq # the wave length in free space
 print 'The wave length in free space is %2.1f meters' % (lambda0)
-
+omega = 2.0 * np.pi * freq # the angular frequency
+k0 = omega ** 2.0 * EMConst.eps0 * EMConst.mu0 # the wave number in free space
 
 
 
@@ -19,8 +20,11 @@ z_dipole = 0.0  # the z coordiante of the dipole
 
 # length of the dipole
 length_dipole = lambda0 / 2 # dipole antennas length is half wave length
-dl_dipole = lambda0 / 10   # the maximum segment length of the dipole
-
+# the segment length of dipole is fixed
+# the maximum segment length of the dipole, this number could be different from
+# the real segment length
+dl_dipole = lambda0 / 10
+rho_dipole = 0.001   # the radius of the dipole
 
 
 
@@ -34,10 +38,33 @@ node_xyz, segment, N_nodes, N_segments = dp.dipole( x_dipole,
                               dl_dipole )
 
 # first calculate the coordinate of segments center point
-segment_center_xyz = (node_xyz[segment[:,0]] + node_xyz[segment[:,1]]) / 2.0
+# then give the y coordinate a little offset
+# make the matching point located on the surface of dipole
+segment_xyz = (node_xyz[segment[:,0]] + node_xyz[segment[:,1]]) / 2.0
+match_xyz = (node_xyz[segment[:,0]] + node_xyz[segment[:,1]]) / 2.0
+match_xyz[:, 1] += rho_dipole
+
+##dl_segment is the length of each segments
+#dl_segment_square = ( node_xyz[segment[:,1]] - node_xyz[segment[:,0]] ) ** 2
+#dl_segment = np.sqrt( np.sum(dl_segment_square, axis = 1) )
+## dl_segment is now a 1D array of floats
+#dl_segment = dl_segment * np.ones((3,1), float)
+
+# "I" is the current filament frow from node0 to node1
+dI =  node_xyz[segment[:,1]] - node_xyz[segment[:,0] ]
+v_seg_to_match = np.reshape(np.tile(match_xyz, N_segments), (N_segments, N_segments, 3)) \
+    - np.tile(segment_xyz, (N_segments, 1, 1))
+r_seg_to_match = np.linalg.norm(v_seg_to_match, axis = 2)
+
+print v_seg_to_match
+print np.tile(segment_xyz, (N_segments, 1, 1))
+print r_seg_to_match
 
 
-print "this is the result"
-print segment_center_xyz
-print node_xyz
+#for x in match_xyz:
+#    # this is the vector point from the segment center to the match point
+#    v_seg_to_match = x - segment_xyz
+#    r_seg_to_match = np.linalg.norm(v_seg_to_match, axis = 1)
+#    print v_seg_to_match
+#    print r_seg_to_match
 
