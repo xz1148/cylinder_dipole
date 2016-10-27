01f0d3cc014094150a264353ec1fc67f00e84ebc
xz1148 <xz1148@york.ac.uk>
2016-10-27 12:54:43 +0100

    neighbour segments of nodes can be calculated

--- main.py
+++ main.py
@@ -30,11 +30,22 @@ def dipole_meshing(x0, y0, z0, x1, y1, z1, n_seg):
     idx_node1 = idx_node[1:n_node]
     mesh = np.stack((idx_seg, idx_node0, idx_node1), axis=1)
 
-    return xyz, mesh
+    # give a list of neighbour segments
+    node_seg_tmp1 = np.empty(n_node, int)
+    node_seg_tmp2 = np.empty(n_node, int)
+    # first column is the neighbour to node0
+    # second column is the neighbour to node1
+    node_seg_tmp1[mesh[:,1]] = mesh[:,0]
+    node_seg_tmp2[mesh[:,2]] = mesh[:,0]
+    node_segment = np.stack((idx_node, node_seg_tmp1,
+                             node_seg_tmp2), axis=1)
+    return xyz, mesh, node_segment
 
 if __name__=="__main__":
-    a, b = dipole_meshing(0,0,0,3.0,2.0,1.0,6)
+    a, b, c = dipole_meshing(0,0,0,3.0,2.0,1.0,6)
+
     print a
     print b
+    print c
 
 
